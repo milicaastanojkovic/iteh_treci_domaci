ba51a00ffa30686cfd275725f6f144955b937731
milicaastanojkovic <stanojkovic.milica46@gmail.com>
2021-01-27 23:49:18 +0100

    Gotova aplikacija

---
+++ src/components/Slider.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+
+export default function Slider(props) {
+    return (
+        <>
+            <label >{props.naziv}</label>
+            <input type="range"
+                onChange={(e) => {
+
+                    const value = e.target.value;
+
+                    props.onChange(value);
+                }}
+                value={props.vrednost}
+                min={props.min} max={props.max} className='form-control' />
+            <input type="number" onChange={(e) => {
+                const value = e.target.value;
+                if (value < props.min) {
+                    props.onChange(props.min);
+                    return;
+                }
+                if (value > props.max) {
+                    props.onChange(props.max);
+                    return;
+                }
+                props.onChange(value);
+            }} value={props.vrednost} className='form-control' />
+        </>
+    )
+}
